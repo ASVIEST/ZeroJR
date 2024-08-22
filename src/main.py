c72f7fc3af5d1d63dbbe77d9f5a90462464048aa
ToserSim <gituhuber@mail.ru>
2024-08-22 19:58:40 +0300

    First steps

---
+++ src/main.py
@@ -0,0 +1,109 @@
+import asyncio
+import os
+import pickle
+from pathlib import Path
+import discord
+import dotenv
+from discord.ext import commands, tasks
+from dpyConsole import Console
+from rich import inspect
+from enum import Enum, auto, StrEnum
+import time
+
+import record
+import builder
+
+import aiohttp
+
+class ClearKind(StrEnum):
+    ALL = auto()
+    THREADS = auto()
+    CHANNELS = auto()
+    CATEGORIES = auto()
+
+
+intents = discord.Intents.all()  # Подключаем "Разрешения"
+intents.message_content = True
+intents.members = True
+intents.presences = True
+
+
+bot = commands.Bot(command_prefix="!", intents=intents)  # Задаём префикс и интент
+my_console = Console(bot)
+
+
+#Эта шиза снизу для распознавания консолью всякой шизы вроде ClearKind
+def path_convert(param):
+    return Path(param) # What the fuck?
+my_console.converter.add_converter(Path, path_convert)
+
+
+def clear_kind_convert(param):
+    return ClearKind(param) # What the fuck?
+my_console.converter.add_converter(ClearKind, clear_kind_convert)
+
+
+def dump(dtree):
+    print(pickle.dumps(dtree))
+    with open("tree.pkl", "wb") as file:
+        pickle.dump(dtree, file)
+
+
+@my_console.command()
+async def create(guild: discord.Guild):
+    start_time = time.time()
+    category_list = []
+    for category in guild.categories:
+        category_obj = record.Category(category)
+        await category_obj.avisitor(category)
+        category_list.append(category_obj)
+        for channel in category.text_channels:
+            for i in channel.threads:
+                inspect(i)
+    await asyncio.to_thread(dump, dtree=category_list)
+    end_time = time.time()
+    print("Create complete!")
+    print(f"Время выполнения: {end_time-start_time} секунд")
+
+
+@my_console.command()
+async def clear(guild: discord.Guild, clear_kind: ClearKind = ClearKind.ALL):
+    #Все или только один
+    if clear_kind in {ClearKind.ALL, ClearKind.THREADS}:
+        for thread in guild.threads:
+            await thread.delete()
+        print("Thread deletion is complete.")
+    if clear_kind in {ClearKind.ALL, ClearKind.CHANNELS}:
+        for channel in guild.channels:
+            await channel.delete()
+        print("Channel deletion is complete.")
+    if clear_kind in {ClearKind.ALL, ClearKind.CATEGORIES}:
+        for category in guild.categories:
+            await category.delete()
+        print("Category deletion is complete.")
+
+
+@my_console.command()
+async def load(guild: discord.Guild, file_name: Path):
+    with open(file_name, "rb") as file:
+        category_list = pickle.load(file)
+    for category in category_list:
+        category_builder = builder.CategoryBuilder(category, bot)
+        await category_builder.build(guild)
+    print("Load complete!")
+
+
+@bot.command()
+async def Гыгы(ctx):
+    print("ГЫГЫ!!!")
+    webhook = await ctx.message.channel.create_webhook(name = ctx.author.display_name)
+    async with ctx.typing():
+        await webhook.send('Hello World', avatar_url=ctx.author.display_avatar.url)
+    await webhook.delete()
+    inspect(webhook)
+
+
+dotenv.load_dotenv()
+token = str(os.getenv("TOKEN"))
+my_console.start()
+bot.run(token)
